d1b4268aa6849d07e1d3246ffd66f998c7e2fb43
jiten kumar <jitendrakumar04@gmail.com>
2018-07-06 21:20:22 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Multiplication two numbers
+sum = float(num1) * float(num2)
+
+# Display the Multiplication
+print('The multiplication of {0} x {1} = {2}'.format(num1, num2,sum))
