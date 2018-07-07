2cd221922e72d57dce543fceea6714f0925f2808
jiten kumar <jitendrakumar04@gmail.com>
2018-07-07 14:18:29 +0530

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,4 +1,5 @@
-''' Program make a simple calculator that can add, subtract, multiply and divide using functions '''
+''' Program make a simple calculator that can add, 
+subtract, multiply and divide using functions '''
 
 # This function adds two numbers 
 def add(x, y):
